8dc7cfd373648ce4119cce07650df60da4af388d
yz-dd <tedz@live.cn>
2019-08-08 10:53:05 -0700

    Updates

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
   $('.sub-menu-trigger').click(function(){
     if ($('#secondary-nav').hasClass('contracted')) {
       $('#secondary-nav').removeClass('contracted').addClass('expanded');
-      $(this).html(jsTranslations.close_trigger + ' <i class="fa fa-caret-up" aria-hidden="true"></i>');
+      $(this).html(jsTranslations.close_trigger + ' <i class="fas fa-caret-up" aria-hidden="true"></i>');
     } else if ($('#secondary-nav').hasClass('expanded')) {
       $('#secondary-nav').removeClass('expanded').addClass('contracted');
-      $(this).html(jsTranslations.open_trigger + ' <i class="fa fa-caret-down" aria-hidden="true"></i>');
+      $(this).html(jsTranslations.open_trigger + ' <i class="fas fa-caret-down" aria-hidden="true"></i>');
     }
   });
 
